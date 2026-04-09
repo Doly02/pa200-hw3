fb34fd8d3d1486804a52f2e6350db58e99ebc31a
Tomáš Dolák <117460208+Doly02@users.noreply.github.com>
2026-04-09 17:11:46 +0200

    [AzureFunctions] Implements Support for Azure Functions.
    - Messages are send into Queue on Azure, and then processed in Azure Function.

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Azure.Storage.Queues;
 
 namespace aspnet_get_started.Controllers
 {
@@ -25,6 +26,49 @@ namespace aspnet_get_started.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SendMessageToQueue(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                TempData["QueueError"] = "Zprava nesmi byt prazdna.";
+                return RedirectToAction("Index");
+            }
+
+            try
+            {
+                await SendMessageToQueueAsync(message);
+                TempData["QueueSuccess"] = "Zprava byla odeslana do fronty.";
+            }
+            catch (Exception ex)
+            {
+                TempData["QueueError"] = ex.Message;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private static async Task SendMessageToQueueAsync(string message)
+        {
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings["AzureStorageConnectionString"];
+            string connectionString = connectionStringSettings?.ConnectionString;
+            string queueName = ConfigurationManager.AppSettings["AzureQueueName"] ?? "jobs";
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ConfigurationErrorsException("V ConnectionStrings.config chybi AzureStorageConnectionString.");
+            }
+
+            var queueClient = new QueueClient(connectionString, queueName);
+
+            await queueClient.CreateIfNotExistsAsync();
+
+            string encodedMessage = Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
+
+            await queueClient.SendMessageAsync(encodedMessage);
         }
     }
 }
